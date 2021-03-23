1ee3e8d3c97bd9b8ff832e9119d791cde8aa3c45
2kminnn <2kminnn@gmail.com>
2021-03-23 21:58:56 +0900

    export 방식(default vs non default)

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 import express from "express"
 
-
+// export 하는 방식의 차이점?
+// export default userRouter
 export const userRouter = express.Router()
 
 userRouter.get("/", (req, res) => res.send('user index'))

354f05f43d56937047b71f2e46a80f938f37eeb8
2kminnn <2kminnn@gmail.com>
2021-04-01 00:21:29 +0900

    ~#2.25 More Controllers

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,4 +1,5 @@
 import {videos} from "../db"
+import routes from "../routes"
 export const home = (req, res) => {
 
     res.render("home", {pageTitle:"Home", videos})
@@ -9,11 +10,20 @@ export const search = (req, res) => {
     // req.query.term과 동일한 표현
     // 얘도 동일함 >> const searchingBy = req.query.term
     const {query:{term: searchingBy}} = req
-    res.render("search", {pageTitle:"Search", searchingBy: searchingBy})
+    res.render("search", {pageTitle:"Search", searchingBy: searchingBy, videos})
     
 }
 
-export const upload = (req, res) => res.render("upload", {pageTitle:"Upload"})
+export const getUpload = (req, res) => res.render("upload", {pageTitle:"Upload"})
+
+export const postUpload = (req, res) => {
+    const {
+        body:{ file, title, description }
+    } = req
+    // To Do: upload and save video
+    res.redirect(routes.videoDetail(324393))
+}
+
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle:"Video Detail"})
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle:"Edit Video"})
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle:"Delete Video"})
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { videos, upload, videoDetail, editVideo, deleteVideo} from "../controller/videoController"
+import { videos, getUpload, postUpload, videoDetail, editVideo, deleteVideo} from "../controller/videoController"
 import routes from "../routes"
 const videoRouter = express.Router()
 
-videoRouter.get(routes.upload, upload)
-videoRouter.get(routes.videoDetail, videoDetail)
+videoRouter.get(routes.upload, getUpload)
+videoRouter.post(routes.upload, postUpload)
+
+videoRouter.get(routes.videoDetail(), videoDetail)
 videoRouter.get(routes.editVideo, editVideo)
 videoRouter.get(routes.deleteVideo, deleteVideo)
 
